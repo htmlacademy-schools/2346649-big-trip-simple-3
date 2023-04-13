d9d91031fe4d1f55f7c56383ddb1ca974497dadf
MikleAKA <mixa1410buz@mail.ru>
2023-04-13 22:35:54 +0300

    module1-task2

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: 'build' },
+      ],
+    }),
+  ],
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
