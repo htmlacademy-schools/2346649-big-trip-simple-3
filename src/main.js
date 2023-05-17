b84e8457493a1168ea8f1bf7ba35765c701e4d15
MikleAKA <mixa1410buz@mail.ru>
2023-05-17 03:15:13 +0300

    module1-task2

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Presenter from './presenter/presenter';
+import FiltersView from './View/filter-view';
+import {render} from './render';
+
+const filters = document.querySelector('.trip-controls__filters');
+render(new FiltersView(), filters);
+
+const container = document.querySelector('.trip-events');
+const tripPresenter = new Presenter({container: container});
+
+tripPresenter.init();
